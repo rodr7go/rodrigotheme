1ea89392bfc2b0109c0f984f5e1b17bb0257d63c
rodrigo <rodrigo@controlmas.com>
2015-05-15 12:34:31 -0500

    primer commit

---
+++ themerodrigo.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en"></html>
+<head>
+  <meta charset="UTF-8"/>
+  <title>Tema de Rodrigo</title>
+  <link rel="stylesheet" href="themerodrigo.css"/>
+</head>
+<body>
+  <h1>Tema de Rodrigo</h1>
+</body>
